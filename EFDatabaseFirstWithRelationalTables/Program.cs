8f994314be7b9e250789561adea3947e1bf669cb
vishaltanawade688@gmail.com <vishaltanawade688@gmail.com>
2022-05-25 08:34:31 +0530

    Fetch Data using LINQ from Multiple Table

--- EFDatabaseFirstWithRelationalTables/Program.cs
+++ EFDatabaseFirstWithRelationalTables/Program.cs
@@ -10,6 +10,17 @@ namespace EFDatabaseFirstWithRelationalTables
     {
         static void Main(string[] args)
         {
-        }
+            GN22ADMDNF001Entities context= new GN22ADMDNF001Entities();
+
+            var SelEmpWithDept = from e in context.Employees
+                                 join d in context.Departments on e.DeptID equals d.DeptId
+                                 select new { EmpID = e.EmpID, EmpName = e.EmpName, CourseDuration = e.CourseDuration, DeptID = e.DeptID, DeptName = d.DeptName, DeptLoc = d.DeptLoc }; // we are using here anonymous method bcz we don't having any class which could handle both combined attributes of dept and emp table
+            Console.WriteLine("EmpID EmpName CourseDuration DeptID DeptName DeptLoc");
+            foreach (var item in SelEmpWithDept)
+            {
+                Console.WriteLine(item.EmpID + " " + item.EmpName + " " + item.CourseDuration + " " + item.DeptID + " " + item.DeptName + " " + item.DeptLoc);
+            }
+
+            }
     }
 }
